24819b5a1361866ef438d02f1e2256a4bc778eda
Tam Myaing <tam@myaing.com>
2011-10-23 15:07:14 -0400

    enable assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,6 @@ Oncapus::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-  # config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
