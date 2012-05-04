653264b934342de9513211581d3f6a0a5252b051
Tam Myaing <tam@myaing.com>
2012-05-04 11:28:01 -0400

    School in paperclip url

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -1,5 +1,9 @@
 # Read the credentials for the data vault: AWS S3
 
+Paperclip.interpolates :school do |attachment, style|
+  attachment.instance.school_id
+end
+
 vault = Vault.last
 
 Vault.default_account = vault.account
