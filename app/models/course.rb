658d870521df2af7024faf0db0f335958f6449bc
Tam Myaing <tam@myaing.com>
2012-05-04 09:53:57 -0400

    Heroku course test

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,7 +7,7 @@ class Course < ActiveRecord::Base
   has_many :attachments, :as => :object
   has_attached_file :image,
     :storage => :s3,
-    :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECR'] },
+    :s3_credentials => { :access_key_id => 'AKIAJMV6IAIXZQJJ2GHQ', :secret_access_key => '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' },
     :path => "courses/:filename",
-    :bucket => ENV['S3_BUCK']
+    :bucket => 'com.neuronicgames.oncampus.test/test'
 end
