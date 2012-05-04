5a216e29ece338b5b66529d72277f00b7e509418
Tam Myaing <tam@myaing.com>
2012-05-04 11:21:30 -0400

    More vault

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,6 +8,6 @@ class Course < ActiveRecord::Base
   has_attached_file :image,
     :storage => :s3,
     :s3_credentials => { :access_key_id => Vault.default_account, :secret_access_key => Vault.default_secret },
-    :path => "schools/:school_id/courses/:filename",
+    :path => "schools/:school/courses/:filename",
     :bucket => Vault.default_folder
 end
