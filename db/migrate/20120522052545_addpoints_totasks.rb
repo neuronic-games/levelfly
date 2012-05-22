ee5135a9463ae6a2e2b7e3f6f6b19fa9de69cfdc
vaibhavjoshi <vaibhavjoshi@cdnsol.com>
2012-05-22 20:29:35 +0530

    changes for new task creation

--- db/migrate/20120522052545_addpoints_totasks.rb
+++ db/migrate/20120522052545_addpoints_totasks.rb
@@ -1,8 +1,9 @@
 class AddpointsTotasks < ActiveRecord::Migration
   def up
-  add_column :tasks, :points, :integer
+    add_column :tasks, :points, :integer, :default=>0
   end
 
   def down
+    remove_column :tasks, :points 
   end
 end
