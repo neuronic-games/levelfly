690c67c0fc09f964146b3c9135d12a0b653a1836
Tam Myaing <tam@myaing.com>
2012-05-30 14:57:32 -0400

    Group cleanups

--- db/migrate/20120529121026_add_type_to_course.rb
+++ db/migrate/20120529121026_add_type_to_course.rb
@@ -1,5 +1,5 @@
 class AddTypeToCourse < ActiveRecord::Migration
   def change
-    add_column :courses, :type, :string
+    add_column :courses, :parent_type, :string, :limit => 1, :default => 'C'
   end
 end
--- db/migrate/20120529180325_change_course_parent_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCourseParentType < ActiveRecord::Migration
-  def up
-    change_column :courses, :parent_type, :string, :limit => 1, :default => 'C'
-    Course.update_all("parent_type = 'C'")
-  end
-
-  def down
-  end
-end
