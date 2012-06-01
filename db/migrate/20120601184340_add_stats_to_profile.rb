054a96861760be865c5bf283ddab226abe8c4747
Tam Myaing <tam@myaing.com>
2012-06-01 16:18:22 -0400

    Removed stats from avatar class

--- db/migrate/20120601184340_add_stats_to_profile.rb
+++ db/migrate/20120601184340_add_stats_to_profile.rb
@@ -2,6 +2,6 @@ class AddStatsToProfile < ActiveRecord::Migration
   def change
     add_column :profiles, :xp, :integer, :default => 0
     add_column :profiles, :badge_count, :integer, :default => 0
-    add_column :profiles, :level, :integer, :default => 0
+    add_column :profiles, :level, :integer, :default => 1
   end
 end
---
+++ db/migrate/20120601201414_remove_stats_from_avatar.rb
@@ -0,0 +1,13 @@
+class RemoveStatsFromAvatar < ActiveRecord::Migration
+  def up
+    remove_column :avatars, :level
+    remove_column :avatars, :points
+    remove_column :avatars, :badge_count
+  end
+
+  def down
+    add_column :avatars, :level, :integer, :default => 1
+    add_column :avatars, :points, :integer, :default => 0
+    add_column :avatars, :badge_count, :integer, :default => 0
+  end
+end
