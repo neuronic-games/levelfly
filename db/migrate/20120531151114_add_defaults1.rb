2b147a9ab51046044ddd309a7bdcb603e97b8126
Tam Myaing <tam@myaing.com>
2012-06-05 17:24:32 -0400

    Fix for boolean in db error migration

--- db/migrate/20120531151114_add_defaults1.rb
+++ db/migrate/20120531151114_add_defaults1.rb
@@ -2,8 +2,8 @@ class AddDefaults1 < ActiveRecord::Migration
   def up
     change_column :courses, :archived, :boolean, :default => false
     change_column :messages, :archived, :boolean, :default => false
-    Course.update_all("archived = 0", "archived is null")
-    Message.update_all("archived = 0", "archived is null")
+    Course.update_all(["archived = ?", false], "archived is null")
+    Message.update_all(["archived = ?", false], "archived is null")
   end
 
   def down
