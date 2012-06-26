1e5e22d42359471ac15fef5ecdf83effa382fea5
Tam Myaing <tam@myaing.com>
2012-06-26 17:58:31 -0400

    Added Rails.application.config.grade_palette

--- config/application.rb
+++ config/application.rb
@@ -48,5 +48,7 @@ module Oncapus
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.grade_palette = true
   end
 end
