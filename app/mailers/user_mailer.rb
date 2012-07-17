39888bdfcfae1460c22a9377baefdf79bf839b15
Tam Myaing <tam@myaing.com>
2012-07-17 12:16:06 -0400

    Added invite email fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,16 +2,14 @@ class UserMailer < ActionMailer::Base
   default :from => "OnCampus Admin <admin@oncampus.com>"
   
   def registration_confirmation(user,sender,course,school)
-    #@user = user
     @user = user
     @sender = sender
     @course = course
-    @school =school
-    subject     " You have been invited to join #{@course.name} #{@course.code} by Prof. #{@sender.full_name} at #{@school.code}"
+    @school = school
+    subject     "You have been invited to join #{@course.name} by #{@sender.full_name} at #{@school.code}"
     recipients  user
     sent_on     Time.now
-    mail(:to => user, 
-    :subject => " You have been invited to join #{@course.name} #{@course.code} by Prof. #{@sender.full_name} at #{@school.code}",)
-    
+    mail( :to => user, 
+          :subject => "You have been invited to join #{@course.name} by #{@sender.full_name} at #{@school.code}")
   end
 end
