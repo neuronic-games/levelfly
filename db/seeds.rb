43a1d56fe0dc1d576148e3c32da514efc52dc6dd
Tam Myaing <tam@myaing.com>
2012-09-14 20:17:28 -0400

    Admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ vault = Vault.create(:vault_type => 'AWS S3', :object_id => school.id, :object_t
 profile = Profile.create(:code => 'DEFAULT', :school => school, :image_file_name => Profile.default_avatar_image, :level => 1)
 default = Avatar.create(:profile => profile, :skin => 3, :body => 'avatar/body/body_3', :head => 'avatar/head/diamond_3', :face => 'avatar/face/latin_male', :hair => 'avatar/hair/short_wavy_5', :hair_back => 'avatar/hair/short_wavy_5_back', :top => 'basic/tops/polo_short_sleeve_blue', :bottom => 'basic/bottoms/trousers_long_brown', :shoes => 'basic/shoes/sneakers_gray')
 
-admin = User.create(:email => "admin@neuronicgames.com", :encrypted_password: "$2a$10$r6oanIl6wuvMUDoyMeF7NeMfMQBkAPSLMEYseOm0mmiz...", :status => 'A')
+admin = User.create(:email => "admin@neuronicgames.com", :encrypted_password => "$2a$10$r6oanIl6wuvMUDoyMeF7NeMfMQBkAPSLMEYseOm0mmiz...", :status => 'A')
 admin_profile = Profile.create(:user => admin, school => school, :full_name => "Neuronic Admin", :image_file_name => "/images/wardrobe/null_profile.png")
 Role.create(:name => "edit_user", :profile => admin_profile)
 Role.create(:name => "modify_rewards", :profile => admin_profile)
