6f00ebb8ee6cb4ff330a9eb7004ef3d9f8ee62eb
Tam <tam@myaing.com>
2013-02-11 16:46:36 -0500

    Different vaults for differetn schools

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,14 @@ class SessionsController < Devise::SessionsController
   
   def identify_school
     default_school = School.find_by_handle("bmcc")
+    session[:school_id] = default_school.id
+    
     school = School.find_by_handle(params[:slug])
-    session[:school_id] = school ? school.id : default_school.id
+    if school
+      School.default_vault = school.vault
+      session[:school_id] = school.id
+    end
+    
     raise ActionController::RoutingError.new('Page Not Found') if params[:slug] and !school
   end
   
