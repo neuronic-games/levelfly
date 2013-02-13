6ab2d84185b29aa9746d62b61f3784c30a18fed2
mukul <mugoyal@enbake.com>
2013-02-13 15:25:50 +0530

    Fixed indexing issue for search

--- db/migrate/20130212125121_add_indices_for_text_search.rb
+++ db/migrate/20130212125121_add_indices_for_text_search.rb
@@ -1,19 +1,28 @@
 class AddIndicesForTextSearch < ActiveRecord::Migration
   def up
-    execute "CREATE INDEX index_courses_on_lower_name_code 
-             ON courses (lower(name), lower(code));"
-    execute "CREATE INDEX index_profiles_on_lower_full_name 
-             ON profiles (lower(full_name));"
-    execute "CREATE INDEX index_tasks_on_lower_name_descr 
-             ON tasks (lower(name), lower(descr));"
-    execute "CREATE INDEX index_messages_on_lower_content_topic 
-             ON messages (lower(content), lower(topic));"
+    execute "CREATE INDEX index_courses_on_lower_name ON courses
+             USING gin(to_tsvector('english', lower(name)));"
+    execute "CREATE INDEX index_courses_on_lower_code ON courses
+             USING gin(to_tsvector('english', lower(code)));"
+    execute "CREATE INDEX index_profiles_on_lower_full_name ON profiles 
+             USING gin(to_tsvector('english',lower(full_name)));"
+    execute "CREATE INDEX index_tasks_on_lower_name ON tasks 
+             USING gin(to_tsvector('english',lower(name)));"
+    execute "CREATE INDEX index_tasks_on_lower_descr ON tasks 
+             USING gin(to_tsvector('english',lower(descr)));"
+    execute "CREATE INDEX index_messages_on_lower_content ON messages 
+             USING gin(to_tsvector('english',lower(content)));"
+    execute "CREATE INDEX index_messages_on_lower_topic ON messages 
+             USING gin(to_tsvector('english',lower(topic)));"
   end
 
   def down
-    execute "DROP INDEX index_courses_on_lower_name_code;"
+    execute "DROP INDEX index_courses_on_lower_name;"
+    execute "DROP INDEX index_courses_on_lower_code;"
     execute "DROP INDEX index_profiles_on_lower_full_name;"
-    execute "DROP INDEX index_tasks_on_lower_name_descr;"
-    execute "DROP INDEX index_messages_on_lower_content_topic;"
+    execute "DROP INDEX index_tasks_on_lower_name;"
+    execute "DROP INDEX index_tasks_on_lower_descr;"
+    execute "DROP INDEX index_messages_on_lower_content;"
+    execute "DROP INDEX index_messages_on_lower_topic;"
   end
 end
