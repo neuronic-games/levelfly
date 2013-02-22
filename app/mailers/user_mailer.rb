f483ccd951b2683ba3a7664deb47bf77f10e039a
Tam <tam@myaing.com>
2013-02-21 22:08:11 -0500

    Private message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ActionMailer::Base
     @course = course
     @school = school
     @message = message_id
-    @link = new_user ? "https://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/system/new_user/?link=#{link}" : "https://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/users/sign_in"
+    @link = new_user ? "http://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/system/new_user/?link=#{link}" : "https://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/users/sign_in"
     @new_user = new_user
     subject     "You have been invited to join #{@course.name} by #{@sender.full_name} at #{@school.code}"
     recipients  user
@@ -16,11 +16,13 @@ class UserMailer < ActionMailer::Base
     mail( :to => user, 
           :subject => "You have been invited to join #{@course.name} by #{@sender.full_name} at #{@school.code}")
   end
+  
   def private_message(user,sender,school,message_content)
     @user = user
     @sender = sender
     @school = school
     @message = message_content
+    @link = "http://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/users/sign_in"
     subject     "#{@sender.full_name} at #{@school.code} has sent you a private message"
     recipients  user
     sent_on     Time.now
