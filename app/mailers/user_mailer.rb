4cea618013e9a5ade6e96fd9001aca376cd73c34
Tam <tam@myaing.com>
2013-03-14 22:16:43 -0400

    WOrding changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ActionMailer::Base
   default :from => "Do Not Reply <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>"
-	
 
   def registration_confirmation(user,sender,course,school,message_id,link,new_user)
     @user = user
@@ -10,12 +9,12 @@ class UserMailer < ActionMailer::Base
     @message = message_id
     @link = "https://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/system/new_user/?link=#{link}"
     @new_user = new_user
-    subject     "Your invitation to join #{@course.name} at #{@school.code}"
+    subject     "[CollegeQuest] Your invitation to join #{@course.name} at #{@school.code}"
     recipients  user
     sent_on     Time.now
     mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
           :to => user, 
-          :subject => "Your invitation to join #{@course.name} at #{@school.code}")
+          :subject => "[CollegeQuest] Your invitation to join #{@course.name} at #{@school.code}")
   end
   
   def private_message(user,sender,school,message_content)
@@ -24,12 +23,12 @@ class UserMailer < ActionMailer::Base
     @school = school
     @message = message_content
     @link = "https://#{Oncapus::Application.config.action_mailer.default_url_options[:host]}/users/sign_in"
-    subject     "#{@sender.full_name} at #{@school.code} has sent you a private message"
+    subject     "[CollegeQuest] #{@sender.full_name} at #{@school.code} has sent you a private message"
     recipients  user
     sent_on     Time.now
     mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
           :to => user, 
-          :subject => "#{@sender.full_name} at #{@school.code} has sent you a private message")
+          :subject => "#[CollegeQuest] {@sender.full_name} at #{@school.code} has sent you a private message")
   end
 	
 	def course_private_message(user,sender,course, msg_content)
@@ -37,7 +36,7 @@ class UserMailer < ActionMailer::Base
 		@sender = sender
 		@message = msg_content
 		@course = course
-		@subject = "#{@sender.full_name} has sent you a message regarding #{@course.code}-#{@course.section}"
+		@subject = "[CollegeQuest] #{@sender.full_name} has sent you a message regarding #{@course.code}-#{@course.section}"
 		mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
 		      :to => @user, 
           :subject => @subject)
