430d54bd15f080f30e6ed322e374b98453d06309
Tam <tam@myaing.com>
2013-03-21 15:38:04 -0400

    Wardrobe script

--- app/models/wardrobe_item.rb
+++ app/models/wardrobe_item.rb
@@ -2,14 +2,14 @@ class WardrobeItem < ActiveRecord::Base
   belongs_to :wardrobe
   belongs_to :parent_item, :class_name => "WardrobeItem"
   
-  def self.AddItem(wardrobe_name, level_0_name, level_1_name, name, item_type, image_file)
+  def self.add(wardrobe_name, level_0_name, level_1_name, name, item_type, image_file)
     wardrobe = Wardrobe.find_by_name(wardrobe_name)
     if wardrobe.nil?
       wardrobe = Wardrobe.create(:name => wardrobe_name, :visible_level => 1, :available_level => 1, :available_date => Date.today, :visible_date => Date.today)
       puts "Wardrobe #{wardrobe.name} (#{wardrobe.id}) created"
     end
     
-    level_0 = WardrobeItem.find(:first, :conditions => ["name = ? and parent_item is null and depth = 0", level_0_name])
+    level_0 = WardrobeItem.find(:first, :conditions => ["name = ? and parent_item_id is null and depth = 0", level_0_name])
     level_1 = WardrobeItem.find(:first, :conditions => ["name = ? and parent_item_id = ? and depth = 1", level_1_name, level_0.id])
     
     sort_order = WardrobeItem.maximum(:sort_order, :conditions => ["parent_item_id = ? and depth = 1", level_0.id])
