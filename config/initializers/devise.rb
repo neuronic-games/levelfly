0059f232ac8ab94da5a76d48a14eb9787beb881c
Tam <tam@myaing.com>
2013-12-25 20:22:05 -0500

    New titles

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "CollegeQuest Administrator (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>"
+  config.mailer_sender = "Levelfly Administrator (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>"
   config.warden do |manager|
     manager.failure_app = CustomFailure
   end
