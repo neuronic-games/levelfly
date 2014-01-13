c881a271df209e7608ae1b8863279ad36275b955
Tam <tam@myaing.com>
2014-01-13 11:35:53 -0500

    Adding major and settings

--- app/models/major.rb
+++ app/models/major.rb
@@ -1,4 +1,18 @@
 class Major < ActiveRecord::Base
   belongs_to :school
   has_many :profile
+  
+  def self.add(school_code, majors)
+    school = School.find_by_code(school_code)
+    return unless school
+    
+    major_list = majors.split(",")
+    major_list.each do |major|
+      name = major.strip
+      next if Major.find(:first, :conditions => {:school_id => school.id, :name => name})
+      
+      Major.create(:school_id => school.id, :name => name)
+    end
+    
+  end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,4 +5,18 @@ class Setting < ActiveRecord::Base
     cut_off_number = setting ? setting.value : 20
   end
 
+  def self.add(school_code, name, value)
+    school = School.find_by_code(school_code)
+    return unless school
+    setting = Setting.find(:first, :conditions => {:object_id => school.id, :object_type => "school", :name => name})
+    if setting.nil?
+      setting = Setting.new
+      setting.object_id = school.id
+      setting.object_type = 'school'
+      setting.name = name
+    end
+    setting.value = value
+    setting.save
+    return setting
+  end
 end
