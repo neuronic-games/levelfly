5579baf42b02f7cedd7bb469e24bcb4fe9244eed
Tam <tam@myaing.com>
2014-02-03 09:44:16 +0630

    Upload wardrobe

--- app/models/wardrobe.rb
+++ app/models/wardrobe.rb
@@ -1,7 +1,7 @@
 class Wardrobe < ActiveRecord::Base
   has_many :wardrobe_items
 
-  def self.add(wardrobe_name, level_0_name, level_1_name, name, item_type, image_file)
+  def self.add(wardrobe_name, level_0_name, level_1_name, name, item_type, image_file, new_name=nil)
     wardrobe = Wardrobe.find(:first, :conditions => ["name like ?", wardrobe_name])
     if wardrobe.nil?
       wardrobe = Wardrobe.create(:name => wardrobe_name, :visible_level => 1, :available_level => 1, :available_date => Date.today, :visible_date => Date.today)
@@ -12,7 +12,12 @@ class Wardrobe < ActiveRecord::Base
     level_1 = WardrobeItem.find(:first, :conditions => ["name like ? and parent_item_id = ? and depth = 1", level_1_name, level_0.id])
     exists = WardrobeItem.find(:first, :conditions => ["name like ? and parent_item_id = ? and depth = 2", name, level_1.id])
     
-    if !exists
+    if exists
+      exists.name = new_name if new_name
+      exists.parent_item = level_1
+      exists.save
+      puts "#{item.name} (#{item.id}) updated"
+    elseif new_name.nil?
       sort_order = WardrobeItem.maximum(:sort_order, :conditions => ["parent_item_id = ? and depth = 1", level_0.id])
       item = WardrobeItem.create(:wardrobe => wardrobe, :parent_item => level_1, :name => name, :item_type => item_type, :image_file => image_file, :sort_order => sort_order+1, :depth => 2)
       puts "#{item.name} (#{item.id}) added to #{level_1.name} (#{level_1.id}), #{level_0.name} (#{level_0.id}), #{wardrobe.name} (#{wardrobe.id})"
@@ -37,4 +42,26 @@ class Wardrobe < ActiveRecord::Base
     
     puts "Wardrobe #{wardrobe.name} (#{wardrobe.id}) unlocked at #{xp} XP"
   end
+
+  def self.unlock_lvl(wardrobe_name, lvl)
+    wardrobe = Wardrobe.find(:first, :conditions => ["name like ?", wardrobe_name])
+    if wardrobe.nil?
+      wardrobe = Wardrobe.create(:name => wardrobe_name, :visible_level => 1, :available_level => 1, :available_date => Date.today, :visible_date => Date.today)
+      puts "Wardrobe #{wardrobe.name} (#{wardrobe.id}) created"
+    end
+
+    lvl_reward = Reward.find(:first, :conditions => {:target_type => 'level', :target_id => lvl})
+    return unless lvl_reward
+
+    exists = Reward.find(:first, :conditions => {:target_type => 'wardrobe', :target_id => wardrobe.id})
+    if exists
+      exists.xp = lvl_reward.xp
+      exists.save
+    else
+      exists = Reward.create(:xp => lvl_reward.xp, :target_type => 'wardrobe', :target_id => wardrobe.id)
+    end
+    
+    puts "Wardrobe #{wardrobe.name} (#{wardrobe.id}) unlocked at level #{lvl_reward.target_id} and #{xp} XP"
+  end
+
 end
---
+++ script/create_wardrobe.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+
+# Property of Levelfly. All rights reserved.
+# Create wardrobe upload script from spreadsheet.
+# https://docs.google.com/spreadsheet/ccc?key=0Aihj5DfFRufddGZvRkYzRlgtNUZFLVVRWVB6ZHpGOGc&usp=drive_web#gid=0
+# Usage: create_wardrobe.rb IN=[csv file] OUT=[output script file] SOURCE=<source image root> TARGET=[target image root]
+# Date: 2014-02-03 Tam
+
+require "csv"
+require "fileutils"
+require "set"
+
+source_path = nil
+target_root = File.expand_path('../../public/images/wardrobe',  __FILE__)
+file_path = File.expand_path('../wardrobe.csv',  __FILE__)
+out_path = File.expand_path('../load_wardrobe.rb',  __FILE__)
+
+ARGV.each do |argv|
+  name, value = argv.split("=")
+  file_path = value if name == 'IN'
+  source_path = value if name == 'SOURCE'
+  target_root = value if name == 'TARGET'
+  out_path = value if name == 'OUT'
+end
+
+puts "Using wardrobe data from #{file_path}"
+puts "Using wardrobe images in #{source_path}"
+puts "Copying wardrobe images to #{target_root}"
+
+reward_name = nil
+item_names = Set.new
+wardrobe_count = 0
+item_count = 0
+
+file = File.new(file_path, "r")
+csv_data = file.read
+
+out  = File.new(out_path, "w")
+out.write "#!/usr/bin/env ruby\n\n"
+out.write "# Property of Levelfly. All rights reserved. Date: #{Date.today}\n"
+
+csv_data.tr("\r", "\n").each_line do | line |
+  next if line.match(/^\s*#/)
+
+  data = CSV.parse_line(line)
+  data.collect! { |x| x ? x.strip : x } 
+  status, level, reward, wardobe_top, wardrobe_sub, item_type, item_name, new_name, img_folder, img_file, icon_folder, icon_file = data
+
+  # We assume that a data row will have a level unlock
+  next if level.to_i == 0
+  
+  if reward and !reward.empty?
+    if reward_name != reward
+      # Update the unlock level at the end of the wardrobe item block
+      out.write "\n# === #{reward} ===\n"
+      out.write "Wardrobe.unlock_lvl('#{reward}', #{level})\n"
+      wardrobe_count += 1
+    end
+    reward_name = reward
+    reward_level = level
+  end
+  reward_folder = reward_name.downcase.strip.sub(/\s+/, '_')
+
+  next if item_name.nil?  # Key field
+  if item_names.include?(item_name)
+    puts "ERROR: #{item_name} must be unique"
+    next
+  end
+  
+  item_name, item_seq = item_name.split(',')
+  next if item_seq  # This script can't handle hair and facial_hair yet
+  item_name.strip!
+  
+  next if item_type.nil?
+  next if icon_file.nil?
+  next if img_file.nil?
+  extname = File.extname(img_file)
+  basename = File.basename(img_file, extname)
+
+  # Wardrobe image
+  target_path = "#{target_root}/avatar/#{reward_folder}/#{img_folder}"
+  FileUtils.mkdir_p(target_path)
+  FileUtils.cp("#{source_path}/avatar/#{img_folder}/#{img_file}", target_path)
+
+  # Wardrobe icon
+  target_path = "#{target_root}/icon/#{reward_folder}/#{img_folder}" # folder should be the same as img_folder
+  FileUtils.mkdir_p(target_path)
+  FileUtils.cp("#{source_path}/icon/avatar/#{icon_folder}/#{icon_file}", "#{target_path}/#{basename}_icon.#{extname}")
+
+  out.write "Wardrobe.add('#{reward_name}', '#{wardobe_top}', '#{wardobe_top}', '#{item_name}', '#{item_type}', '#{reward_folder}/#{img_folder}/#{basename}'"
+  out.write ", '#{new_name}'" if new_name
+  out.write ")\n"
+  
+  item_count += 1
+
+  puts "#{reward_name}: #{new_name ? new_name : item_name} (#{img_file})"
+
+end
+
+puts "#{item_count} items in #{wardrobe_count} wardrobes"
+puts "Output: #{out_path}"
+
+file.close
+out.close
