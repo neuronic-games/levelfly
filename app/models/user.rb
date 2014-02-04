2ba63d95dc8c50b51ff5542bb0412d9216ed5510
Ethan James <ethan@Ethan-Jamess-iMac.local>
2014-02-04 16:11:31 -0500

    jiggle

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
+ 
   has_one :profile
   
   def friends
