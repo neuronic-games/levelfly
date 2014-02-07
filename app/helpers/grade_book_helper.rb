8ccbf143377abc4ed7ee9e85a60851c527a7c3ea
Ethan James <ethan@Ethan-Jamess-iMac.local>
2014-02-07 17:15:57 -0500

    category percentage weight

--- app/helpers/grade_book_helper.rb
+++ app/helpers/grade_book_helper.rb
@@ -1,11 +1,12 @@
 module GradeBookHelper
-  
+  include ActionView::Helpers::NumberHelper
+
   def load_caregory_name(task_id)
     task = Task.find(task_id)
     if task.category
-      return "  (#{task.category.name}) "
+      return "  (#{task.category.name} #{number_with_precision(task.category.percent_value, :precision => 2) || 0}%) "
     end
-   return "  (Uncategorized)"
+   return "  (Uncategorized 0%)"
   end
   
 end
