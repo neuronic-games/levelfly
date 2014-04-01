bfe953230ae93411e3f03fe9a76b93de6fdb2675
Ethan James <ethan@Ethan-Jamess-iMac.local>
2014-04-01 17:42:10 -0400

    trying to get devise to add my message on reconfirmation, without great success

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,11 +1,22 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def show
-    reconfirmed = current_user && current_user.pending_reconfirmation?
-    super
+    u = User.find_by_confirmation_token(params[:confirmation_token])
+    reconfirmed = u.pending_reconfirmation? if u
 
-    if reconfirmed
-      flash[:notice] = "Your account email has been successfully changed."
-      flash[:email_confirmation] = true
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      if reconfirmed
+        flash[:notice] = "Your account email has been successfully changed."
+        flash[:email_confirmation] = true
+      else
+        set_flash_message(:notice, :confirmed)
+      end
+
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
   end
 end
