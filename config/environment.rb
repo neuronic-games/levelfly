c0650263753bcb64ac74312620a20b0d77fa55cd
Tam <tam@myaing.com>
2014-04-29 03:09:01 -0400

    Bumped version number to 1.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.4.1'
+APP_VERSION = '1.5'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
