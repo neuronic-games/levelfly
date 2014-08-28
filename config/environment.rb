038e15a796f3c426489045af5e7f4431e8ff52d3
Tam <tam@myaing.com>
2014-08-27 21:30:17 -0400

    Bumped version number to 1.6.1

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.6'
+APP_VERSION = '1.6.1'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
