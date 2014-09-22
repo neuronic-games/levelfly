413a6f2212edc5cd452f65f45d5d77c048bb3883
Anton Brychak <a.v.brychak@gmail.cov>
2014-09-22 13:12:44 +0300

    fix group tabs issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Oncapus::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -36,8 +36,8 @@ Oncapus::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.logger = Logger.new(STDOUT)
-  config.log_level = :debug
+  #config.logger = Logger.new(STDOUT)
+  #config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
