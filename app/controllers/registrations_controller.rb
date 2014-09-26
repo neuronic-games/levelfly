74417f339d525ac6c50e4137fafe533f0971a4aa
Anton Brychak <a.v.brychak@gmail.com>
2014-09-26 13:16:18 +0300

    debug stuff removed. acount creation fixed (resend confirmation email problem)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,19 +5,16 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! START USER CREATE'
     @user = User.find_by_email(params[:user][:email])
     @school = school
     @role = nil
 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHECKING PARAMS'
     if params[:school]
       school_code = params[:school][:code].upcase
     else
       school_code = ''
     end
 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHECK IF USER UNCONFIRMED'
     if @user && !@user.confirmed?
       if @user.unconfirmed_email
         @user.send_confirmation_instructions
@@ -29,13 +26,11 @@ class RegistrationsController < Devise::RegistrationsController
       return redirect_to new_user_session_url
     end
 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHECK NAME PARAMS'
     if params[:user][:full_name].length == 0
       flash[:notice] = ['Enter your full name.']
       return redirect_to new_registration_path(resource_name)
     end
 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHECK SCHOOL'
     if school_code.length > 0
       if @school = School.find_by_teacher_code(school_code)
         @role = RoleName.find_by_name('Teacher')
@@ -51,26 +46,16 @@ class RegistrationsController < Devise::RegistrationsController
         return redirect_to new_registration_path(resource_name)
       end
     end
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHECK USER AGAIN'
     if @user
       unless @user.valid_password? params[:user][:password]
         flash[:notice] = ["An account with this email already exists. Enter the correct password or click \"Forgot your password?\" above."]
         return redirect_to new_registration_path(resource_name)
       end
     else
-      puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! USER NEW HERE !!!!!!!'
       @user = User.new(params[:user])
     end
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    puts @user.inspect
-    puts @user.errors.inspect
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
     @user.default_school = @school
     if @user.save
-      puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! USER SAVED'
-      puts @user.inspect
-      puts @user.errors.inspect
-      puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
       profile = Profile.create_for_user(@user.id, @school.id)
       profile.full_name = params[:user][:full_name]
 
