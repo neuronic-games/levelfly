e463d19c7809ec7045c623fbd6ce5e487619e275
Anton Brychak <a.v.brychak@gmail.com>
2014-09-26 12:49:12 +0300

    trying to use devise default settings for confirmations

--- config/initializers/email_confirmation.rb
+++ config/initializers/email_confirmation.rb
@@ -1,6 +1,6 @@
-module Devise::Models::Confirmable
-  # Override Devise's own method. This one is called only on user creation, not on subsequent email modifications.
-  def send_on_create_confirmation_instructions
-    UserMailer.welcome_email(self).deliver
-  end
-end
+# module Devise::Models::Confirmable
+#   # Override Devise's own method. This one is called only on user creation, not on subsequent email modifications.
+#   def send_on_create_confirmation_instructions
+#     UserMailer.welcome_email(self).deliver
+#   end
+# end
