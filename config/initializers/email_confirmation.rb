0e7270ed26cf860b86d4d9c65a377107ec39a506
Anton Brychak <a.v.brychak@gmail.com>
2014-09-29 13:24:08 +0300

    -

--- config/initializers/email_confirmation.rb
+++ config/initializers/email_confirmation.rb
@@ -1,5 +1,5 @@
 module Devise::Models::Confirmable
-#   # Override Devise's own method. This one is called only on user creation, not on subsequent email modifications.
+  # Override Devise's own method. This one is called only on user creation, not on subsequent email modifications.
   def send_on_create_confirmation_instructions
     UserMailer.welcome_email(self).deliver
   end
