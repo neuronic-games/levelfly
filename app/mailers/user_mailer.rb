b909db6e8352719e48d549390c7e91735a282943
Anton Brychak <a.v.brychak@gmail.com>
2014-09-29 17:23:32 +0300

    all threads removed from user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ActionMailer::Base
           :reply_to => "Do Not Reply <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>"
 
   def registration_confirmation(user,sender,course,school,message_id,link,new_user)
-    UserMailer.thready do
       @user = user
       @sender = sender
       @course = course
@@ -17,7 +16,6 @@ class UserMailer < ActionMailer::Base
       mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
             :to => user,
             :subject => @subject)
-    end
   end
 
   def welcome_email(user)
@@ -28,7 +26,6 @@ class UserMailer < ActionMailer::Base
   end
 
   def school_invite(user, current_profile)
-    UserMailer.thready do
       @user = user
       @sender = current_profile
       @school = current_profile.school
@@ -40,11 +37,9 @@ class UserMailer < ActionMailer::Base
       mail(:from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
            :to => user.email,
            :subject => @subject)
-    end
   end
 
   def private_message(user,sender,school,message_content)
-    UserMailer.thready do
       @user = user
       @sender = sender
       @school = school
@@ -56,11 +51,9 @@ class UserMailer < ActionMailer::Base
       mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
             :to => user,
             :subject => @subject)
-    end
   end
 
   def wardrobe_unlock_message(user,sender,school,message_content)
-    UserMailer.thready do
       @user = user
       @sender = sender
       @school = school
@@ -72,11 +65,9 @@ class UserMailer < ActionMailer::Base
       mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
             :to => user,
             :subject => @subject)
-    end
   end
 
   def course_private_message(user, sender, school, course, msg_content)
-    UserMailer.thready do
       @user = user
       @sender = sender
       @school = school
@@ -87,11 +78,9 @@ class UserMailer < ActionMailer::Base
       mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
             :to => @user,
             :subject => @subject)
-    end
   end
 
   def new_badge(receiver,sender,school,course,badge_name)
-    UserMailer.thready do
       @receiver = receiver
       email = receiver.user.email
       @sender = sender
@@ -105,7 +94,6 @@ class UserMailer < ActionMailer::Base
       mail( :from => "#{@sender.full_name} (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>",
             :to => email,
             :subject => @subject)
-    end
   end
 
   private
