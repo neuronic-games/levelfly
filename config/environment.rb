65b28c068f45b3b03ad80715889c4d1550ec293b
Tam Myaing <tam@myaing.com>
2015-12-10 15:46:35 -0500

    Testing staging deploy

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.7.2'
+APP_VERSION = '1.7.4 beta'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
