734257a3d35568254650b0d78f9637ce4728ce56
Tam Myaing <tam@myaing.com>
2016-01-27 00:07:17 +0630

    Support for high scores

--- app/models/feat.rb
+++ app/models/feat.rb
@@ -1,4 +1,38 @@
-class GameScoreLeader < ActiveRecord::Base
+class Feat < ActiveRecord::Base
   belongs_to :game
   belongs_to :profile
+
+  @@login = 0
+  cattr_accessor :login
+
+  @@xp = 1
+  cattr_accessor :xp
+
+  @@score = 2
+  cattr_accessor :score
+
+  @@badge = 3
+  cattr_accessor :badge
+
+  @@rating = 4
+  cattr_accessor :rating
+  
+  before_save :check_xp
+  
+  private
+  
+  # Make sure the constraints on xp and score are met before saving
+  def check_xp
+    if self.progress_type == Feat.xp
+      # xp cannot be more than a 1000
+      return false if self.progress > 1000
+      
+      # xp cannot go down
+      last_xp = game.get_xp(self.profile_id)
+      return false if self.progress < last_xp
+    end
+
+    return true
+  end
+  
 end
