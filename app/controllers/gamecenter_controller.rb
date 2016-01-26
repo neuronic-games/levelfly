77fffa5467b3196815d688eed631c2c1166b72fb
Tam Myaing <tam@myaing.com>
2016-01-26 12:52:54 +0630

    Improving game connect

--- app/controllers/gamecenter_controller.rb
+++ app/controllers/gamecenter_controller.rb
@@ -1,6 +1,6 @@
 class GamecenterController < ApplicationController
   layout 'main'
-  before_filter :authenticate_user!, :except => [:status, :authenticate, :get_current_user]
+  before_filter :authenticate_user!, :except => [:status, :connect, :authenticate, :get_current_user]
 
   def status
     message = "All OK"
