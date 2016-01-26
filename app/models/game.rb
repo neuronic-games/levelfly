6a5a7d7d596d11ffcfca1e8a5c860d81ca3fd9a3
Tam Myaing <tam@myaing.com>
2016-01-26 12:19:40 +0630

    Added game handle

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,15 @@
 class Game < ActiveRecord::Base
   validates :handle, :uniqueness => true
+  
+  after_create :generate_handler
+  
+  private
+  
+  def generate_handler
+    md5 = Digest::MD5.new
+    md5 << "#{self.id}"
+    self.handle = md5.hexdigest
+    save
+    return self.handle
+  end
 end
