df48626110acced8057788f0c07d1f80f4099733
Tam Myaing <tam@myaing.com>
2016-01-26 14:24:12 +0630

    Added Feat model

---
+++ db/migrate/20160126000001_create_feats.rb
@@ -0,0 +1,16 @@
+class CreateFeats < ActiveRecord::Migration
+  def change
+    create_table :feats do |t|
+      t.integer :game_id,     :null => false
+      t.integer :profile_id,  :null => false
+      
+      t.integer :progress
+      t.integer :progress_type
+      t.string  :level
+      
+      t.timestamps
+    end
+
+    add_index :feats, [:profile_id, :game_id]
+  end
+end
