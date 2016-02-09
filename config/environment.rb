649126d5e003735801d9a23b8e6e8efeeffeb616
Tam <tam@neuronicgames.com>
2016-02-09 23:47:38 +0000

    beta 6

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.7.4 beta 5'
+APP_VERSION = '1.7.4 beta 6'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
