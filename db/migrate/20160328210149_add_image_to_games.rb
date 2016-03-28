428608ad9a0a2637276fa3e4475a2f8518877be3
Tam Myaing <tam@myaing.com>
2016-03-28 17:19:34 -0400

    Added paperclip image to games

---
+++ db/migrate/20160328210149_add_image_to_games.rb
@@ -0,0 +1,13 @@
+class AddImageToGames < ActiveRecord::Migration
+  def self.up
+    add_column :games, :image_file_name, :string
+    add_column :games, :image_content_type, :string
+    add_column :games, :image_file_size, :integer
+  end
+
+  def self.down
+    remove_column :games, :image_file_name
+    remove_column :games, :image_content_type
+    remove_column :games, :imagee_file_size
+  end
+end
