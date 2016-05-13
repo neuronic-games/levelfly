1bcf9765b348dbdbb01514a4aebc202ccaa5ba88
Tam Myaing <tam@myaing.com>
2016-05-13 13:33:46 -0500

    Using a different setting for email URL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Levelfly Administrator (Do Not Reply) <donotreply@#{Oncapus::Application.config.action_mailer.default_url_options[:host]}>"
+  config.mailer_sender = "Levelfly Administrator (Do Not Reply) <donotreply@#{ENV['URL']}>"
   config.warden do |manager|
     manager.failure_app = CustomFailure
   end
