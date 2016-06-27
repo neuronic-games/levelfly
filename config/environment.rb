d0eac6ecd73f6f2252d7f116ffaa0c68d5a580d6
Tam Myaing <tam@myaing.com>
2016-06-27 04:45:12 -0400

    1.7.7

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.7.6'
+APP_VERSION = '1.7.7'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
