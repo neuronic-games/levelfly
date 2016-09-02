3a09871c609ec7296214dc201f5ea0b333b07da0
Tam <tam@myaing.com>
2016-09-02 21:11:28 +0000

    Basic game stats

--- config/routes.rb
+++ config/routes.rb
@@ -458,6 +458,7 @@ Oncapus::Application.routes.draw do
   get "gamecenter/edit_game"
   put "gamecenter/update_game"
   post "gamecenter/update_game"
+  get "gamecenter/view_game_stats"
   get "gamecenter/download"
   get "gamecenter/support"
   get "gamecenter/all_badges"
