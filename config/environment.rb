59ef62d87b7c2d4cbe5d3729044618ff4b71cea3
Tam <tam@myaing.com>
2016-09-22 17:22:15 +0000

    #750 Private user is shows up as private on forum post to course owner

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.8.5'
+APP_VERSION = '1.8.6'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
