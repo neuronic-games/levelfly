d8f5f6374d11f954e0618ebcf740330de5d17001
Tam Myaing <tam@myaing.com>
2017-05-17 12:50:50 -0400

    Ask users for the privacy settings

---
+++ db/migrate/20170517160031_change_profile_privacy_default.rb
@@ -0,0 +1,11 @@
+class ChangeProfilePrivacyDefault < ActiveRecord::Migration
+  def up
+    change_column :profiles, :is_public, :boolean, :default => nil
+    Profile.update_all("is_public = null", "is_public is false")
+  end
+
+  def down
+    change_column :profiles, :is_public, :boolean, :default => true
+    Profile.update_all("is_public = true", "is_public is null")
+  end
+end
