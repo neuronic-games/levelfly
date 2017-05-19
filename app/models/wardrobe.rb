a7ad329151eabe875b9a34b80d040688a38bc3d3
Tam Myaing <tam@myaing.com>
2017-05-19 15:26:14 -0400

    Fixes to wardrobe script

--- app/models/wardrobe.rb
+++ app/models/wardrobe.rb
@@ -11,7 +11,7 @@ class Wardrobe < ActiveRecord::Base
     level_0 = WardrobeItem.find(:first, :conditions => ["name like ? and parent_item_id is null and depth = 0", level_0_name])
     level_1 = WardrobeItem.find(:first, :conditions => ["name like ? and parent_item_id = ? and depth = 1", level_1_name, level_0.id])
     if level_1.nil?
-      puts "ERROR: #{level_1_name} does not exit"
+      puts "ERROR: #{level_1_name} in #{level_0_name} does not exit"
       return
     end
     exists = WardrobeItem.find(:first, :conditions => ["name like ? and parent_item_id = ? and depth = 2", name, level_1.id])
