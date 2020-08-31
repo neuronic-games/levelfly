7ddc9341af7d559c594758947ca7932a98fe3d31
3wc <3wc.git@doesthisthing.work>
2020-08-31 18:23:25 +0200

    Tweak migration so that DB migrations run

--- db/migrate/20200425020515_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20200425020515_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -9,7 +9,7 @@ AddMissingUniqueIndices.class_eval do
     add_index :tags, :name, unique: true
 
     remove_index :taggings, :tag_id if index_exists?(:taggings, :tag_id)
-    remove_index :taggings, [:taggable_id, :taggable_type, :context]
+    #remove_index :taggings, [:taggable_id, :taggable_type, :context]
     add_index :taggings,
               [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],
               unique: true, name: 'taggings_idx'
