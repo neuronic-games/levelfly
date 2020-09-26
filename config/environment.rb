bec83e428f96a3025123b4b9594db39f73b1612b
Tam Myaing <tam@myaing.com>
2020-09-25 20:31:23 -0400

    Improved the forum message loading for games. The query will no longer kill the server.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = '1.8.21'
+APP_VERSION = '1.8.22'
 
 # Initialize the rails application
 Oncapus::Application.initialize!
