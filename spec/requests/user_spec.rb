0cf2d0453ec1baea4ab23adca9eab13d0535b42c
3wc <3wc.github@doesthisthing.work>
2025-09-23 18:46:09 -0400

    test: Test users index and set_invite_codes

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# NOTE: In the UI as "People"
+
+RSpec.describe 'Users', type: :request do
+  before(:all) do
+    @user = User.first
+    @school_demo = School.find_by!(handle: 'demo')
+    @profile = @user.profiles.first
+  end
+
+  context 'GET /index' do
+    it 'should redirect to login if unauthenticated' do
+      get url_for controller: 'users', action: :index
+      expect(response).to redirect_to '/users/sign_in'
+    end
+
+    it 'should render index page' do
+      sign_in @user
+      get url_for(controller: 'users', action: :index),
+          xhr: true
+      expect(response.body).to include @profile.full_name
+    end
+  end
+
+  context 'POST /set_invite_codes' do
+    it 'should redirect to login if unauthenticated' do
+      post url_for(controller: 'users', action: :set_invite_codes),
+           params: { student_code: Faker::Alphanumeric.alpha, teacher_code: Faker::Alphanumeric.alpha }
+      expect(response).to redirect_to '/users/sign_in'
+    end
+
+    it 'should set invite codes' do
+      sign_in @user
+      post url_for(controller: 'users', action: :set_invite_codes),
+           params: { student_code: Faker::Alphanumeric.alpha, teacher_code: Faker::Alphanumeric.alpha }
+      response_parsed = JSON.parse(response.body)
+      expect(response_parsed['status']).to be true
+
+      # TODO: Test that duplicate codes can't be set
+    end
+  end
+end
