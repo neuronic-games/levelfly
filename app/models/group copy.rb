4ee83241a7e485a9d94a82bd3afae5f7e0acf233
3wc <3wc.github@doesthisthing.work>
2025-09-25 16:53:47 -0400

    chore: Remove duplicate `group copy.rb`..
    ..because it was blowing up zeitwerk

--- app/models/group copy.rb
+++
@@ -1,8 +0,0 @@
-class App < ActiveRecord::Base
-  belongs_to :school
-
-  has_attached_file :image,
-                    path: 'schools/:school/apps/:id/:filename'
-  # FIXME: https://stackoverflow.com/a/21898204/14269772
-  do_not_validate_attachment_file_type :image
-end
