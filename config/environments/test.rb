f1b638224667db591ef484558103e4dd580e6d93
3wc <3wc.github@doesthisthing.work>
2025-09-27 00:14:07 -0400

    test: Don't queue jobs during testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Oncapus::Application.configure do
   Pusher.secret = 'MY_TEST_SECRET'
 
   require 'pusher-fake/support/base'
+
+  # Disable job queuing during testing https://github.com/collectiveidea/delayed_job?tab=readme-ov-file
+  Delayed::Worker.delay_jobs = false
 end
