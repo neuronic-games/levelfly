a03f7fa1c6115d8a69df6c03439293d615eb559f
3wc <3wc.github@doesthisthing.work>
2025-09-26 22:44:24 -0400

    test: view_group_setup and send_group_invitation specs

--- spec/requests/group_spec.rb
+++ spec/requests/group_spec.rb
@@ -17,6 +17,24 @@ RSpec.describe 'Groups', type: :request do
                          profile_type: 'M')
   end
 
+  context 'when GET /view_group_setup' do
+    it 'redirects to login if unauthenticated' do
+      get url_for(controller: 'course', action: :view_group_setup),
+          params: { id: group_one.id }
+      expect(response).to redirect_to '/users/sign_in'
+    end
+
+    it 'shows group setup page' do
+      sign_in user_one
+
+      get url_for(controller: 'course', action: :view_group_setup),
+          params: { id: group_one.id }
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include CGI.escapeHTML(group_one.name)
+    end
+  end
+
   context 'when POST /filter' do
     it 'redirects to login if unauthenticated' do
       post url_for(controller: 'course', action: :filter),
@@ -35,14 +53,42 @@ RSpec.describe 'Groups', type: :request do
 
       post url_for(controller: 'course', action: :filter),
            params: { filter: 'M', section_type: Course.parent_type_group }
+      expect(response).to have_http_status(:ok)
       expect(response.body).to include group_one.name
       expect(response.body).not_to include group_two.name
 
       post url_for(controller: 'course', action: :filter),
            params: { filter: 'A', section_type: Course.parent_type_group }
 
+      expect(response).to have_http_status(:ok)
       expect(response.body).to include group_one.name
       expect(response.body).to include group_two.name
+    end
+  end
+
+  context 'when POST /send_group_invitation' do
+    it 'redirects to login if unauthenticated' do
+      post url_for(controller: 'course', action: :send_group_invitation),
+           params: { id: group_one.id }
+      expect(response).to redirect_to '/users/sign_in'
+    end
+
+    it 'sends a group invitation to a non-member' do
+      user_two = create(:user, default_school: school_demo)
+      profile_two = create(:profile, user: user_two, school: school_demo)
+
+      sign_in user_two
+
+      post url_for(controller: 'course', action: :send_group_invitation),
+           params: { id: group_one.id }
+
+      expect(response).to have_http_status(:ok)
+
+      response_parsed = JSON.parse(response.body)
+
+      expect(response_parsed['status']).to be true
+      expect(profile_two.participants.first.profile_id).to eq(profile_two.id)
+      expect(profile_two.participants.first.profile_type).to eq('P')
     end
   end
 end
