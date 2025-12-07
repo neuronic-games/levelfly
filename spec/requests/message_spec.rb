7e2c401b6771fe0815170b0273347cd5c518b40c
3wc <3wc.github@doesthisthing.work>
2025-12-07 00:10:51 -0500

    test: Catch crashes in /add_friend_card

--- spec/requests/message_spec.rb
+++ spec/requests/message_spec.rb
@@ -53,10 +53,10 @@ RSpec.describe 'Messages', type: :request do
 
   context 'when POST /course/toggle_priority_message' do
     let(:params) { { id: message_one.id } }
+    let(:url) { url_for(controller: 'course', action: :toggle_priority_message, params: params) }
 
     it 'redirects to login if unauthenticated' do
-      post url_for(controller: 'course', action: :toggle_priority_message),
-           params: params
+      post url
       expect(response).to redirect_to '/users/sign_in'
     end
 
@@ -64,8 +64,7 @@ RSpec.describe 'Messages', type: :request do
       skip 'Need to verify if this is intended behaviour, see note in course_controller.rb'
       sign_in user_two
 
-      post url_for(controller: 'course', action: :toggle_priority_message),
-           params: params
+      post url
 
       expect(response).to have_http_status(:forbidden)
       message_one.reload
@@ -75,8 +74,7 @@ RSpec.describe 'Messages', type: :request do
     it 'toggles message starred status' do
       sign_in user_one
 
-      post url_for(controller: 'course', action: :toggle_priority_message),
-           params: params
+      post url
 
       expect(response).to have_http_status(:ok)
       expect(json_body['starred']).to be true
@@ -94,6 +92,26 @@ RSpec.describe 'Messages', type: :request do
 
       message_two.reload
       expect(message_two.starred).to be false
+    end
+  end
+
+  context 'when POST /message/add_friend_card' do
+    let!(:user_two) { create(:user, default_school: school_demo) }
+    let!(:profile_two) { create(:profile, user: user_two, school: school_demo) }
+    let(:params) { { profile_id: profile_two.id } } 
+    let(:url) { url_for(controller: 'message', action: :add_friend_card, params: params) }
+
+    it 'redirects to login if unauthenticated' do
+      post url
+      expect(response).to redirect_to '/users/sign_in'
+    end
+
+    it 'renders "add friend" card' do
+      sign_in user_one
+
+      post url
+
+      require 'pry'; binding.pry
     end
   end
 end
